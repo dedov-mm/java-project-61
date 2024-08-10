1aa98a2014f2bc92ae38d76f9712c497e3cbc512
Mikhail Dedov <mdedov@gmail.com>
2024-08-11 01:21:10 +0300

    add Makefile and create a user interaction method

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,10 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
 tasks.test {
     useJUnitPlatform()
 }
