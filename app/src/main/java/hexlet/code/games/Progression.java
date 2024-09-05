3775fd64c775cbfa7c80588b14c0c1004790fcac
Mikhail Dedov <mdedov@gmail.com>
2024-09-05 18:01:21 +0300

    fix magic numbers

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -19,17 +19,25 @@ public class Progression {
     }
 
     private static String[] generateRoundData() {
-        final var startNumber = Utils.generateNumber(1, 100);
-        final var stepNumber = Utils.generateNumber(2, 5);
-        final var missingPosition = Utils.generateNumber(1, 10);
-        final var numberSeries = new int[10];
+        final var amountOfNumbers = 10;
+        final var minStartNumber = 1;
+        final var maxStartNumber = 100;
+        final var minStepNumber = 2;
+        final var maxStepNumber = 5;
+        final var minMissingPosition = 1;
+        final var maxMissingPosition = amountOfNumbers;
 
-        var questionAndAnswer = calculate(numberSeries, startNumber, stepNumber, missingPosition);
+        final var startNumber = Utils.generateNumber(minStartNumber, maxStartNumber);
+        final var stepNumber = Utils.generateNumber(minStepNumber, maxStepNumber);
+        final var missingPosition = Utils.generateNumber(minMissingPosition, maxMissingPosition);
+
+        var questionAndAnswer = calculate(amountOfNumbers, startNumber, stepNumber, missingPosition);
 
         return questionAndAnswer;
     }
 
-    private static String[] calculate(int[] numberSeries, int startNumber, int stepNumber, int missingPosition) {
+    private static String[] calculate(int amountOfNumbers, int startNumber, int stepNumber, int missingPosition) {
+        var numberSeries = new int[amountOfNumbers];
         var question = new StringBuilder();
         var answer = 0;
 
